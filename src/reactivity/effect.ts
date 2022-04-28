6a6ade90e26122b7936a9f9143caab858714017b
xiaoxiong <286536753@qq.com>
2022-04-29 01:05:27 +0800

    实现effect的scheduler功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -2,7 +2,7 @@
 class ReactiveEffect {
     private _fn: any;
 
-    constructor(fn) {
+    constructor(fn, public scheduler?) {
         this._fn = fn;
     }
     run() {
@@ -38,14 +38,18 @@ export function trigger(target, key){
     let dep = depMap.get(key);
 
     for (const effect of dep) {
-        effect.run();
+        if(effect.scheduler){
+            effect.scheduler();
+        }else{
+            effect.run();
+        }
     }
 }
 
 let activeEffect;
-export function effect(fn) {
+export function effect(fn,options: any = {}) {
     //fn
-    const _effect = new ReactiveEffect(fn);//面向对象思想
+    const _effect = new ReactiveEffect(fn, options.scheduler);//面向对象思想
 
     _effect.run();
 
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -19,7 +19,7 @@ describe("effect",()=>{
         expect(nextAge).toBe(12);
     });
 
-    it('',()=>{
+    it('should return runner when call effect',()=>{
         //effect(fn) -> function(runner) ->fn -> return
         let foo = 10;
         const runner = effect(()=>{
@@ -31,5 +31,38 @@ describe("effect",()=>{
         const r = runner();
         expect(foo).toBe(12);
         expect(r).toBe('foo');
+    });
+
+    it('scheduler',()=>{
+        //1.通过effect的第二个参数给定得到,一个scheduler的 fn
+        //2.effect 第一次执行的时候 还会执行fn
+        //3.当 响应式对象 set update 不会执行 fn 而是执行scheduler
+        //4.如果说当执行 runner的时候,会再次的执行fn
+        let dummy;
+        let run: any;
+        // jest.fn()可以理解为创建一个函数,这个函数会收集一些信息.比如:传入的参数,调用的次数 等 . 主要用于后续的验证
+        const scheduler = jest.fn(()=>{
+            run = runner;
+        });
+
+        const obj = reactive({ foo: 1 });
+        const runner = effect(()=>{
+            dummy = obj.foo;
+        },
+        { scheduler }
+        );
+
+        expect(scheduler).not.toHaveBeenCalled();//用于判断函数是否有被调用过
+        expect(dummy).toBe(1);
+        //should be called on first trigger
+
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);//函数被调用的次数
+        //should not run yet
+        expect(dummy).toBe(1);
+        //manually run 
+        run();
+        //should have run
+        expect(dummy).toBe(2);
     })
 })
