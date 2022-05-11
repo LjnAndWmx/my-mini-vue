e1c9ff5bfe39c903b0740433721d9525cd72e237
xiaoxiong <286536753@qq.com>
2022-05-12 00:21:01 +0800

    实现readonly功能

---
+++ src/reactivity/baseHandlers.ts
@@ -0,0 +1,39 @@
+import { track, trigger } from "./effect";
+
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+
+function createGetter(isReadonly = false){
+    return function get(target, key){
+        const res = Reflect.get(target,key);
+        if(!isReadonly){
+            track(target, key);
+        }
+        console.log('get开始');
+        console.log(res);
+        return res;
+    }
+}
+
+function createSetter(){
+    return function set(target, key, value){
+        const res = Reflect.set(target, key, value);
+        trigger(target, key);
+        console.log('set开始');
+        console.log(res);
+        return res;
+    }
+}
+export const mutableHandlers = {
+    get,
+    set
+}
+
+export const readonlyHandlers = {
+    get: readonlyGet,
+    set(target, key, value) {
+        console.warn(`key:${key} set s失败 因为 target是readonly`, target);
+        return true;
+    }
+}
